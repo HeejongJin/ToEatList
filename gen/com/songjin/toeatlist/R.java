1ea9c1f6497ddcd775c2a8b30910e7ba5852689b
Jun-ho, Song <jun9358@gmail.com>
2014-01-17 15:31:17 +0900

    Add left navigation drawer

--- gen/com/songjin/toeatlist/R.java
+++ gen/com/songjin/toeatlist/R.java
@@ -8,6 +8,9 @@
 package com.songjin.toeatlist;
 
 public final class R {
+    public static final class array {
+        public static final int left_drawer_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,21 +25,28 @@ public final class R {
     }
     public static final class drawable {
         public static final int ab_solid_pinkactionbar=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int content_frame=0x7f090001;
+        public static final int drawer_layout=0x7f090000;
+        public static final int left_drawer=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int left_drawer_list_item=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int left_drawer_close=0x7f050004;
+        public static final int left_drawer_open=0x7f050003;
     }
     public static final class style {
         /** 
@@ -60,12 +70,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int PinkActionBar=0x7f060003;
-        public static final int PinkTheme=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int PinkActionBar=0x7f070003;
+        public static final int PinkTheme=0x7f070002;
     }
 }
--- src/com/songjin/toeatlist/MainActivity.java
+++ src/com/songjin/toeatlist/MainActivity.java
@@ -1,13 +1,44 @@
 package com.songjin.toeatlist;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity
 {
 	private static final String TAG = "ToEatList";
+	
+	private String[] mLeftDrawerTitles;
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mLeftDrawerToggle;
+	private ListView mLeftDrawerList;
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener
+	{
+	    @Override
+	    public void onItemClick(AdapterView parent, View view, int position, long id)
+	    {
+	    	
+	    }
+	}
+	
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mLeftDrawerToggle.syncState();
+    }
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -16,7 +47,77 @@ public class MainActivity extends Activity
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mLeftDrawerTitles = getResources().getStringArray(R.array.left_drawer_array);
+		mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+		
+		// Create new drawer toggle
+		mLeftDrawerToggle =
+			new ActionBarDrawerToggle
+			(
+				this,						/* Host activity */
+				mDrawerLayout,				/* DrawerLayout object */
+				R.drawable.ic_drawer,		/* Navigation drawer icon */
+				R.string.left_drawer_open,	/* Open drawer description */
+				R.string.left_drawer_close	/* Close drawer description */
+			)
+			{
+	            /** Called when a drawer has settled in a completely open state. */
+	            public void onDrawerOpened(View drawerView)
+	            {
+	                super.onDrawerOpened(drawerView);
+	            }
+	            
+				/** Called when a drawer has settled in a completely closed state. */
+	            public void onDrawerClosed(View view)
+	            {
+	                super.onDrawerClosed(view);
+	            }
+			};
+
+	    // Set the drawer toggle as the DrawerListener
+	    mDrawerLayout.setDrawerListener(mLeftDrawerToggle);
+	    
+	    getActionBar().setDisplayHomeAsUpEnabled(true);
+	    getActionBar().setHomeButtonEnabled(true);
+	    
+		mLeftDrawerList = (ListView)findViewById(R.id.left_drawer);
+		
+        // Set the adapter for the list view
+		mLeftDrawerList.setAdapter
+		(
+			new ArrayAdapter<String>
+			(
+				this,
+				R.layout.left_drawer_list_item,
+				mLeftDrawerTitles
+			)
+		);
+		
+        // Set the list's click listener
+		mLeftDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 	}
+	
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mLeftDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mLeftDrawerToggle.onOptionsItemSelected(item))
+        {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
