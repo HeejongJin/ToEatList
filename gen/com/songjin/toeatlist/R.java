7942d18573fc18280ebba9737add9f79e524dcf7
Jun-ho, Song <jun9358@gmail.com>
2014-01-18 13:20:06 +0900

    Add and apply background style of layer

--- gen/com/songjin/toeatlist/R.java
+++ gen/com/songjin/toeatlist/R.java
@@ -25,14 +25,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ab_solid_pinkactionbar=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_facebook=0x7f020002;
-        public static final int ic_googleplus=0x7f020003;
-        public static final int ic_kakaotalk=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int ic_locationinfo=0x7f020006;
-        public static final int ic_twitter=0x7f020007;
-        public static final int ic_urllink=0x7f020008;
+        public static final int bg_style=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_facebook=0x7f020003;
+        public static final int ic_googleplus=0x7f020004;
+        public static final int ic_kakaotalk=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_locationinfo=0x7f020007;
+        public static final int ic_twitter=0x7f020008;
+        public static final int ic_urllink=0x7f020009;
     }
     public static final class id {
         public static final int URL_text=0x7f090008;
